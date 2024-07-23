5ba1359250ab9896a9110d1f908e7a0cf958568e
박창호 <pch5521@gmail.com>
2024-07-23 10:24:30 +0900

    test: "jenkins 연동 테스트 중"

--- Back-End/src/test/java/com/safefoodtruck/sft/SftApplicationTests.java
+++ Back-End/src/test/java/com/safefoodtruck/sft/SftApplicationTests.java
@@ -1,13 +1,13 @@
-package com.safefoodtruck.sft;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SftApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.safefoodtruck.sft;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SftApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
