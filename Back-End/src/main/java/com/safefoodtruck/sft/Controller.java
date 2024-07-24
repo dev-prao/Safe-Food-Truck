9ba796166c58876c1914762b58f01102c1e5be92
조용훈 <sjsin0905@naver.com>
2024-07-24 22:38:41 +0900

    a

--- Back-End/src/main/java/com/safefoodtruck/sft/Controller.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    @GetMapping("/api")
+    @GetMapping("/")
     public String home() {
         return "test";
     }
