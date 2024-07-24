5e93822a0d7217baf70e29d328bf2e91c5a1a895
박창호 <pch5521@gmail.com>
2024-07-24 10:50:15 +0900

    feat: "StoreRepository 생성"

--- Back-End/src/main/java/com/safefoodtruck/sft/store/repository/StoreRepository.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/store/repository/StoreRepository.java
@@ -1,5 +1,10 @@
 package com.safefoodtruck.sft.store.repository;
 
-public interface StoreRepository {
+import com.safefoodtruck.sft.store.domain.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Long> {
 
 }
