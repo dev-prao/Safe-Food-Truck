187f589cb9b5cdf067e91185425c23622eb4ae80
박창호 <pch5521@gmail.com>
2024-07-24 09:59:22 +0900

    feat: "MenuImage 연관관계 추가"

--- Back-End/src/main/java/com/safefoodtruck/sft/menu/domain/MenuImage.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/menu/domain/MenuImage.java
@@ -1,10 +1,13 @@
 package com.safefoodtruck.sft.menu.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -34,4 +37,8 @@ public class MenuImage {
 
     @Column(name = "saved_path")
     private String savedPath;
+
+    @OneToOne(mappedBy = "menuImage", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
 }
