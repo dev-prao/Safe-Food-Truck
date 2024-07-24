53b02a77d551cf848eb39e2d2a43e2012b0e7b84
박창호 <pch5521@gmail.com>
2024-07-24 10:50:48 +0900

    chore: "dto 패키지 구조 변경"

--- Back-End/src/main/java/com/safefoodtruck/sft/menu/dto/request/MenuCreateRequestDto.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/menu/dto/request/MenuCreateRequestDto.java
@@ -1,4 +1,4 @@
-package com.safefoodtruck.sft.menu.dto;
+package com.safefoodtruck.sft.menu.dto.request;
 
 public record MenuCreateRequestDto(long storeId, String name, int price, String description) {
 }
