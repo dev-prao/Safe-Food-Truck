aaa233202ee70bc62689c4fbc72d75a473d6c8c8
박창호 <pch5521@gmail.com>
2024-07-24 10:50:23 +0900

    feat: "MenuRepository 생성"

--- Back-End/src/main/java/com/safefoodtruck/sft/menu/repository/MenuRepository.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/menu/repository/MenuRepository.java
@@ -1,5 +1,9 @@
 package com.safefoodtruck.sft.menu.repository;
 
-public interface MenuRepository {
+import com.safefoodtruck.sft.menu.domain.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface MenuRepository extends JpaRepository<Menu, Long> {
 }
