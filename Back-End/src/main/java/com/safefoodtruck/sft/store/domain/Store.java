97e98cb58b606ffc86f9d1c3c98a5c27ddecdbd7
박창호 <pch5521@gmail.com>
2024-07-24 10:11:10 +0900

    feat: "Store 연관관계 추가"

--- Back-End/src/main/java/com/safefoodtruck/sft/store/domain/Store.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/store/domain/Store.java
@@ -1,14 +1,18 @@
 package com.safefoodtruck.sft.store.domain;
 
 import com.safefoodtruck.sft.member.domain.Member;
+import com.safefoodtruck.sft.menu.domain.Menu;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,10 +32,6 @@ public class Store {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "store_id")
     private long id;
-
-    @OneToOne(mappedBy = "store")
-    @JoinColumn(name = "email")
-    private Member owner;
 
     @Column(name = "store_type")
     private String storeType;
@@ -53,4 +53,11 @@ public class Store {
 
     @Column(name = "isOpen")
     private boolean isOpen;
+
+    @OneToOne(mappedBy = "store")
+    @JoinColumn(name = "email")
+    private Member owner;
+
+    @OneToMany(mappedBy = "store")
+    private List<Menu> menuList = new ArrayList<>();
 }
