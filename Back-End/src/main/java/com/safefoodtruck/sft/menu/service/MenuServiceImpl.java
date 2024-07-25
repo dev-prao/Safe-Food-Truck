34035c63971f5f0a065489e8859915ecb17e3933
박창호 <pch5521@gmail.com>
2024-07-25 17:37:41 +0900

    feat: "MenuServiceImpl 추가"

--- Back-End/src/main/java/com/safefoodtruck/sft/menu/service/MenuServiceImpl.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/menu/service/MenuServiceImpl.java
@@ -1,5 +1,34 @@
 package com.safefoodtruck.sft.menu.service;
 
-public class MenuServiceImpl implements MenuService  {
+import com.safefoodtruck.sft.menu.domain.Menu;
+import com.safefoodtruck.sft.menu.dto.request.MenuListRegistRequestDto;
+import com.safefoodtruck.sft.menu.repository.MenuRepository;
+import com.safefoodtruck.sft.store.domain.Store;
+import com.safefoodtruck.sft.store.exception.StoreNotFoundException;
+import com.safefoodtruck.sft.store.repository.StoreRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MenuServiceImpl implements MenuService {
+
+    private final MenuRepository menuRepository;
+    private final StoreRepository storeRepository;
+
+    @Override
+    public void registMenu(int storeId, MenuListRegistRequestDto menuListRegistRequestDto) {
+        Store store = storeRepository.findById(storeId)
+            .orElseThrow(StoreNotFoundException::new);
+
+        menuListRegistRequestDto.menuRegistRequestDtos().forEach(dto -> {
+            Menu menu = Menu.of(dto.name(), dto.price(), dto.description());
+            menu.addStore(store);
+            menuRepository.save(menu);
+        });
+    }
 }
